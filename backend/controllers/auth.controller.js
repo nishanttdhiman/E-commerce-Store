158eea5879d5eb9fbab688ad166ddac2d047c8cb
nishanttdhiman <nishantdhiman0209@gmail.com>
2026-03-28 06:26:56 +0530

    signup completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,41 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60, // 7days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent XSS attack -> Cross site scripting
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attacks, cross-site request forgery
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevent XSS attack -> Cross site scripting
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attacks, cross-site request forgery
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,11 +51,19 @@ export const signup = async (req, res) => {
 
     const user = await User.create({ name, email, password });
 
+    // authenticate user
+    const { accessToken, refreshToken } = generateToken(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
     return res.status(201).json({
       message: "User created Successfully",
       user: {
+        id: user._id,
         name: user.name,
         email: user.email,
+        role: user.role,
       },
     });
   } catch (error) {
